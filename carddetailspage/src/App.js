03af19e326f642bda9723f08638cccd6acfba46a
firatkill <firatkill@icloud.com>
2022-10-24 17:04:03 +0300

    Done

--- carddetailspage/src/App.js
+++ carddetailspage/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import LeftSideComponent from "./Components/LeftSideComponent/LeftSideComponent";
+import RightSideComponent from "./Components/RightSideComponent/RightSideComponent";
+import Card from "./Components/UI/Card/Card";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <LeftSideComponent />
+      <RightSideComponent />
+      <Card />
     </div>
   );
 }
